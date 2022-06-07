5e350509ecc3bc6b0c2682c01378e6d77a82b2eb
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-07 08:40:24 +0530

    Add files via upload

---
+++ ArrayTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class ArrayTest
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = {'G', 'a', 'u', 'a', 'v',
+                         'j', 'a', 'd', 'h',
+                                 'a', 'v'};
+            Console.Write("Array printing using for loop = ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Array printing using foreach loop = ");
+            foreach (char ch in arr)
+            {
+                Console.Write(ch);
+            }       
+        }
+    }
+}
---
+++ LoopTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class LoopTest
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("Using While Loop:Gaurav Jadhav ");
+                i++;
+            }
+            for (i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Using For Loop :Gaurav Jadhav ");
+            }
+        }
+    }
+}
